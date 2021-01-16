380278722b4c209c7b19c12f353bf6f2a0d0dd1e
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-01-16 21:22:10 +0100

    céer une application react

--- mon-app-to-do-list/src/index.js
+++ mon-app-to-do-list/src/index.js
@@ -1,3 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+import App from "./components/App";
+ReactDOM.render(<App />, document.getElementById("root"));
